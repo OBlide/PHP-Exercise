250f1be9c1aa82d7cb65aee4f9d9cafd61936ee7
Oscar Blide <oscarblide@gmail.com>
2019-01-18 11:36:41 +0100

    Fixed one bug

--- fourth.php
+++ fourth.php
@@ -3,14 +3,12 @@
 <h1>Functions</h1>
 
 <?php
-    function $calculate_sum($a, $b){
+    function calculate_sum($a, $b){
         return $a+$b;
     }
 $result=calculate_sum(3,5);
 echo 'The sum is '.$result;
 ?>
-
-
 
 
 <?php include "footer.php"; ?>
