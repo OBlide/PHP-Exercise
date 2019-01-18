34b29cc57386b4bef8b323e8cf8b53f70d6fe48c
Oscar Blide <oscarblide@gmail.com>
2019-01-18 11:07:12 +0100

    Added test files for the form

--- third.php
+++ third.php
@@ -2,8 +2,26 @@
 
 <h1>Forms</h1>
 
-    <?php
-    
-    ?>
+<h2>POST-method</h2>
+
+    <form class="" action="test1.php" method="post">
+    <label for="">Firstname</label><br>
+    <input type="text" id="fn" name="fname" value=""><br>
+    <input for="">Lastname</label><br>
+    <input type="text" id="ln" name="lname" value=""><br>
+    <input type="submit" name="" value="Send"><br>
+    </form>
+
+<h2>GET-method</h2>
+
+    <form class="" action="test1.php" method="get">
+    <label for="">Firstname</label><br>
+    <input type="text" id="fn" name="fname" value=""><br>
+    <input for="">Lastname</label><br>
+    <input type="text" id="ln" name="lname" value=""><br>
+    <input type="submit" name="" value="Send"><br>
+    </form>
+
+
 
 <?php include "footer.php"; ?>
